be5fe32e093e65b1c3c3cf075de26b8d4a7d2636
Guilherme B. <guilhermebanha.dev@gmail.com>
2024-11-24 04:36:30 +0000

    feat: new cloth form

--- src/js/dashboard/edit_profile.js
+++ src/js/dashboard/edit_profile.js
@@ -21,7 +21,7 @@ function showNavbar() {
 // Validate formulary based on user input and default criteria.
 const form = document.querySelector('body > main > div.login-form > form');
 
-form.addEventListener('submit', function(event) {
+form?.addEventListener('submit', function(event) {
 
   event.preventDefault();
 
@@ -35,9 +35,9 @@ form.addEventListener('submit', function(event) {
   else if(!isEmailValid(email)) {triggerError();}
   else if(!isPassowrdValid(passwd)) {triggerError();}
   else if(newpasswd) {if(!isNewPasswordValid(passwd, newpasswd)) triggerError(); else{
-    let pessoa = { name : name, email : email, passwd : passwd,newpasswd : newpasswd }; triggerSuccess(pessoa);
+    let pessoa = { name : name, email : email, passwd : passwd,newpasswd : newpasswd }; triggerSuccess(pessoa,form);
   }}
-  else if(!newpasswd){ let pessoa = { name : name, email : email, passwd : passwd }; triggerSuccess(pessoa);}
+  else if(!newpasswd){ let pessoa = { name : name, email : email, passwd : passwd }; triggerSuccess(pessoa,form);}
 })
 
 function isNameValid(someoneName) {
@@ -68,9 +68,103 @@ function triggerError(){
   k.className = "red"
 }
 
-function triggerSuccess(pessoa){
+function triggerSuccess(pessoa,form){
   let k = document.getElementById("error");
   k.style.display = "flex";
-  k.className = "green"
+  k.className = "green";
+  let l = document.getElementById("error-paragraph");
+  l.innerHTML = "Dados válidos, atualizações feitas";
   console.log(pessoa)
+  form.submit();
+}
+
+
+
+
+function showError(isError = false){
+  const svg = document.getElementById("error-context");
+  const form = document.getElementById("form-holder");
+  form.style.display = 'none';
+  const p = document.getElementById("title-error-box");
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+  const error_box = document.getElementById("error-box");
+  error_box.style.display = 'flex';
+
+  const svgError = "../../../../public/imgs/others/error.svg"
+  const svgSuccess = "../../../../public/imgs/others/success.svg";
+  
+  p.innerHTML = isError === false ? "Change made sucessfuly!" : "Change got errrors!";
+  svg.setAttribute("src", isError === false ? svgSuccess : svgError);
+}
+
+function ShowForm(){
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+  
+}
+
+/* Validate clothes Form */
+const clothesForm = document.querySelector("#clothForm");
+
+clothesForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(clothesForm)
+
+  const clothName = formData.get("jacket-name")
+  const weather = formData.get("weather-type")
+
+   if (!isNameValid(clothName)) {
+    showError(true);
+    clothesForm.reset();
+    return;
+  }
+
+  showError();
+  document.querySelector("#clothForm").submit();
+})
+// clothesForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   console.log(clothesForm)
+
+//   const formData = new FormData(clothesForm)
+
+//   const clothName = formData.get("jacket_name")
+
+//   console.log(clothName)
+
+//   if (!isNameValid(clothName)) {
+//     showError(true);
+//     event.reset();
+//     return;
+//   }
+
+//   showError();
+//   event.submit();
+// });
+
+
+function ChangeClothes(){
+  let fileH = document.getElementById("fileH");
+  fileH.click();
+  const formimgcloth = document.getElementById("form-cloth-img");
+
+  fileH.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+
+    if (file && (file.type === 'image/png' || file.type === 'image/jpeg')) {
+      const reader = new FileReader();
+
+      reader.onload = function(e) {
+        formimgcloth.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      alert('Please select a valid image.');
+    }
+  });
+
 }
